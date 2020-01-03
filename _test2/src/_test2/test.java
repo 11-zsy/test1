71201a0bc1abb7322807688a301d3bb59f12bf23
000 <000@192.168.21.73>
2020-01-03 11:53:27 +0800

    1

--- _test2/src/_test2/test.java
+++ _test2/src/_test2/test.java
@@ -5,5 +5,8 @@ public class test {
 	public static void main(String[] args) {		
 		String s = "abc";				
 		int a = 10;
+		System.out.println(a);
+		System.out.println(s);
+		System.out.println(a+s);
 	}
 }
